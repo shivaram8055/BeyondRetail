116a89b829e55be269053d1ef2b7a4c890cbbe31
shiva <shivamusku8055@gmail.com>
2023-11-01 23:14:46 +0530

    navbar deleted

--- beyondRetail/src/Components/NavBar.jsx
+++ beyondRetail/src/Components/NavBar.jsx
@@ -4,50 +4,7 @@ import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 
 const NavBar = () => {
-  return (
-    <div className="flex px-navbarPadX py-navbarPadY">
-      <div className="flex">
-        <div className="pr-190px">BEYONDRETAIL</div>
-        <ul className="flex">
-          <li>
-            <a href="#" className="pr-48px">
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="#" className="pr-48px">
-              Contact
-            </a>
-          </li>
-          <li>
-            <a href="#" className="pr-48px">
-              About
-            </a>
-          </li>
-          <li>
-            <a href="#" className="pr-148px">
-              SignUp
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div className="flex">
-        <div className=" bg-slate-300 text-sm px-20px py-7px">
-          What are you looking for?{<SearchIcon />}
-        </div>
-        <div className="flex pl-24px">
-          <ul className="flex">
-            <li className="pr-16px">
-              <FavoriteBorderIcon />
-            </li>
-            <li>
-              <ShoppingCartOutlinedIcon />
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default NavBar;
