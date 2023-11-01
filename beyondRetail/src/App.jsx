285f4a537de6c30017ac26b02a412a1ccc6948ba
shiva <shivamusku8055@gmail.com>
2023-11-01 23:09:22 +0530

    deleted footer

--- beyondRetail/src/App.jsx
+++ beyondRetail/src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import NavBar from "./Components/NavBar";
-import Footer from "./Components/footer";
 
 const App = () => {
   return (
     <>
       <NavBar />
-      <Footer />
     </>
   );
 };
