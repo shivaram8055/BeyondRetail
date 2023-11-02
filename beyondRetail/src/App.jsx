17a037bec2cd0a3ce93f0823e798618631422ba8
shyam85858 <indalashyamvinay@gmail.com>
2023-11-03 01:29:07 +0530

    ok

--- beyondRetail/src/App.jsx
+++ beyondRetail/src/App.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import NavBar from "./Components/NavBar";
 import ProductComp from "./Components/ProductComp";
+import airpods from "./assets/MTJV3.jpeg";
+import NewArrivals from "./Components/NewArrivals";
+import Categories from "./Components/Categories";
+
 const App = () => {
   return (
     <>
-      <ProductComp
+      {/* <ProductComp
         productImg={airpods}
         mrp={"₹24900.00"}
         title={
           "AirPods Pro (2nd generation) with MagSafe Charging Case (USB‑C)"
         }
-      />
+      /> */}
+      <Categories />
 
       {/* <NavBar />
       <Footer /> */}
