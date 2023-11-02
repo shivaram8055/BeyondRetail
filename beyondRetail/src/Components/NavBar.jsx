a9a7daf3ad988eaaf3ac3740343fa6a3ffd4c9fd
shiva <shivamusku8055@gmail.com>
2023-11-02 14:29:11 +0530

    sond

--- beyondRetail/src/Components/NavBar.jsx
+++ beyondRetail/src/Components/NavBar.jsx
@@ -3,6 +3,7 @@ import SearchIcon from "@mui/icons-material/Search";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import ArrowForwardIosOutlinedIcon from "@mui/icons-material/ArrowForwardIosOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 
 import { Link } from "react-router-dom";
 
@@ -25,10 +26,17 @@ const NavBar = () => {
             <a href="">SignUp</a>
           </nav>
         </div>
-        <div className="flex gap-8">
-          <SearchIcon />
-          <div className="flex justify-between gap-3 md:gap-7">
+        <div className="flex gap-4 md:gap-6 items-center">
+          <div>
+            <SearchIcon />
+          </div>
+          <div className="hidden md:block">
             <FavoriteBorderIcon />
+          </div>
+          <div>
+            <AccountCircleOutlinedIcon />
+          </div>
+          <div>
             <ShoppingCartOutlinedIcon />
           </div>
         </div>
